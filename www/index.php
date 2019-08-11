7e652957fca3f5139cb6c95585237f904d354b1b
George <salixs@mail.ru>
2019-08-11 18:48:32 +0300

    123

---
+++ www/index.php
@@ -0,0 +1,280 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="/css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <title>Hello, world!</title>
+<style>
+  ::-webkit-input-placeholder {color:red; opacity:1;}/* webkit */
+  ::-moz-placeholder          {color:red; opacity:1;}/* Firefox 19+ */
+  :-moz-placeholder           {color:red; opacity:1;}/* Firefox 18- */
+  :-ms-input-placeholder      {color:red; opacity:1;}/* IE */
+</style>
+  </head>
+  <body> 
+      <header class="head1">
+              <div class="heellow container">
+                  <nav class="navbar navbar">
+                    <a class="navbar-brand"><img src="/img/2.png"></a>
+                      <ul class="nav nav ">
+                          <li class="nav-item acrive"><a class="nav-link font-weight-bold" href="">HOME</a></li>
+                          <li class="nav-item"><a style="text-decoration:none;" class="nav-link font-weight-bold" href="">ABOUT</a></li>
+                          <li class="nav-item"><a style="text-decoration:none;" class="nav-link font-weight-bold" href="">SERVISE</a></li>
+                          <li class="nav-item"><a style="text-decoration:none;" class="nav-link font-weight-bold" href="">GALLERY</a></li>
+                          <li class="nav-item"><a style="text-decoration:none;" class="nav-link font-weight-bold" href="">CONTACT</a></li>
+
+                      </ul>
+                  </nav>
+              </div>
+              <div class=container>
+              <div class="row">
+                  <div class="col-md-8">
+                    <div class="yyyy1">
+                      <p   style="color:black">SELL YOUR</p>
+                      <p   style="color:white">CAR NOW</p>
+                    </div>  
+                    <div class="text">
+                        <p class="font-italic">Learn Ipsum is simply dummy text of the printing and<br>typesetting industry. Lorem Ipsum has been the<br>industry standart dummy text ever sine the 1500s</p>
+                    </div>   
+
+                       </div>
+
+                      <div class="col-md-3">
+                        <div class="blind">
+                          
+                          <p class="hellomom font-weight-bold">
+                              REQUEST A QUOTE
+                          </p>
+                        
+                        <form class="form-horizontal">
+                          <div class="yyyt12">
+                            
+                            <input type="text" style="margin-left:23px; margin-bottom:20px" class="form-control" required placeholder="Full Name">
+                        
+                            <input type="email" style="margin-left:23px; margin-bottom: 20px" class="form-control" required placeholder="Email Adress">
+                         
+                           <input type="tel" style="margin-left:23px; margin-bottom: 20px" class="form-control" required placeholder="Phone Number">
+
+                            <input type="text" style="margin-left:23px; margin-bottom: 20px;" class="form-control" required placeholder="Country">
+                          </div>
+                           <p class="uterp">I accept the terms & conditions</p>
+                        <button style=" margin-left:23px; margin-bottom: 20px; width:300px; font-size: 19px; font-family:'Montserrat', sans-serif;" type="submit" class="btn btn-danger font-weight-bold">Send</button>
+                        </form>
+
+                        </div>
+                          
+                      </div>
+              </div>
+</div>
+      </header>
+
+
+  <section class="container">
+     <div class="row" style="margin-top:40px; font-size:50px">
+      <div>
+       <p class="text-danger font-weight-bold" style="font-family:'Montserrat', sans-serif; letter-spacing: -3px"> OUR</p>
+      </div>
+      <div style="padding-left:15px; font-family:'Montserrat', sans-serif; letter-spacing: -3px" class="font-weight-bold">
+        SERVICES
+      </div>
+     </div>
+
+      <div class="row ttt125">
+        <div class="col-md-3">
+          <center><img src="/img/3.png"></center>
+          <p style="margin-top:18px" >Making your trip<br>beautiful and easier</p>
+        </div>
+         <div class="col-md-3">
+         <center> <img src="/img/4.png"></center>
+          <p style="margin-top:22px">Tools to help you<br>succeed tommorow</p>
+        </div>
+         <div class="col-md-3">
+          <center><img src="/img/5.png"></center>
+          <p style="margin-top:5px">Ideas that will blow<br>you out the blue</p>
+        </div>
+         <div class="col-md-3">
+          <center><img src="/img/6.png"></center>
+          <p style="margin-top:9px">Navigate your path<br>to beautiful world</p>
+        </div>
+
+        
+
+      </div>
+
+
+     
+  </section>
+        
+       <aside class="bg2" >
+
+        <div class="container">
+          <div class="row">
+            <div class="col-md-7">
+          <p class="mt-3"style=" font-family:'Montserrat', sans-serif;color:white; font-size:18px; line-height: 28px">We are in business for over 6 years providing amazing servise to client<br>and people love them to the core. View out story to know more</p>
+        </div>
+        <div class="col-md-2 offset-sm-2">
+          <a class="btn btn-danger mt-4 font-weight-bold" style="width:220px; height:51px;  font-family:'Montserrat', sans-serif; color:white;padding-top:12px">OUR STORY</a>
+        </div>
+        </div>
+        </div>
+      </aside>
+
+      <article class="container bg3">
+       <p class="text-center font-weight-bold" style="font-size:50px;line-height: 80px; letter-spacing: -3px;  ">PRICING</p> 
+       <div class="row eeq">
+        <div class="col-md-4" style="margin-top:70px">
+          <p class="text-center" style="font-size:20px; line-height: 19px">Started</p>
+          <p class="text-center text-danger font-weight-bold"style="font-size:60px; line-height: 70px">FREE</p>
+          <p class="text-center" style="font-size:18px; line-height: 28px">Free Service<br>Multipe Accounts<br>Management No</p>
+          <p class="text-center" style="line-height: 4px">--</p>
+          <p class="text-center" style="line-height: 4px">--</p>
+          <center><a style="width:190px; height:37px;  font-family:'Montserrat', sans-serif; color:white;padding-top:8px;" class="btn btn-dark font-weight-bold">TRY</a></center>
+        </div>
+        <div class="col-md-4"style="margin-top:1px">
+          <img src="/img/7.png" width="70px" style="margin-left:40px">
+          <p class="text-center" style="font-size:20px; line-height: 7px">Bisiness</p>
+          <p class="text-center text-danger font-weight-bold"style="font-size:60px; line-height: 70px">$97</p>
+          <p class="text-center" style="font-size:18px; line-height: 28px">Free Service<br>Multipe Accounts<br>Management No<br>Unlimited Data</p>
+          <p class="text-center" style="line-height: 4px">--</p>
+          
+          <center><a style="width:190px; height:37px;  font-family:'Montserrat', sans-serif; color:white;padding-top:8px;" class="btn btn-danger font-weight-bold">SIGN UP</a></center>
+        </div>
+        <div class="col-md-4" style="margin-top:72px">
+          <p class="text-center" style="font-size:20px; line-height: 2px">Professional</p>
+          <p class="text-center text-danger font-weight-bold"style="font-size:60px; line-height: 70px">$297</p>
+          <p class="text-center" style="font-size:18px; line-height: 28px">Free Service<br>Multipe Accounts<br>Management No<br>Unlimited Data<br>Whatever You Need</p>
+         
+          <center><a style="width:190px; height:37px;  font-family:'Montserrat', sans-serif; color:white;padding-top:8px;" class="btn btn-dark font-weight-bold">SIGN UP</a></center>
+        </div>
+       </div>
+      </article>
+
+
+      <article class="container">
+        <div class="row rrre2">
+          <div class="col-md-6 ">
+            <p class="text-center font-weight-bold " style="letter-spacing: -2px;font-size:50px">TESTIMONIAL</p>
+            <div class="eqqt421s">
+              <div class="row">
+                <div class="col-md-8 ">
+                  <p style="font-size:18px; line-height: 28px">I have involved with this company<br>for ages and just want to say this is a<br>great work by designcoon</p>
+                  
+                  <p class="font-weight-bold" style="font-size:18px; line-height: 28px">DJ Bravo - Frequent Travelera</p>
+                </div>
+                <div class="col-md-2">
+              <img src="/img/8.png" >
+                 </div>
+              </div>
+               <div class="row">
+                <div class="col-sm-8 ">
+                  <p  class="text-danger" style="font-size:18px; line-height: 28px">I have been involved with this company<br>for ages and just</p>
+                  
+                  <p class="font-weight-bold" style="font-size:18px; line-height: 28px">DJ Bravo - Frequent Travelera</p>
+                </div>
+                <div class="col-md-1 ">
+              <img src="/img/9.png">
+                 </div>
+              </div>
+          </div>
+        </div>
+          <div class="col-md-6">
+            <p class="text-center text-danger font-weight-bold " style="letter-spacing: -2px;font-size:50px">GALLERY</p>
+           <div class="la2">
+            <div class="row ewq">
+
+              <div class="col-xs-4">
+                <img src="/img/10.png">
+                
+              </div>
+              <div class="col-xs-4">
+                <img  src="/img/11.png">
+                
+              </div>
+              
+              <div class="col-xs-4">
+                <img  src="/img/12.png" >
+                
+              </div>
+            </div>
+              <div class="row ewq1">
+
+              <div class="col-xs-4">
+                <img  src="/img/13.png" >
+                
+              </div>
+              <div class="col-xs-4">
+                <img s" src="/img/14.png">
+                
+              </div>
+              
+              <div class="col-xs-4">
+                <img  src="/img/15.png" >
+                
+              </div>
+              
+            </div>
+</div>
+ <p class="text-center font-weight-bold" style="font-size: 18px; margin-top:10px; "><a class="pre" href="">VIEW MORE PICTURES</a></p>
+          </div>
+
+        </div>
+
+
+      </article>
+      <footer style="background-color: #130e27; height: auto; margin-top:50px">
+        <div class="container">
+          <div class="row" > 
+            <div class="col-md-7" style="margin-top: 90px; margin-bottom: 70px">
+              <p style="color:white; font-size: 18px">All Rights Reserved | <?php echo date ('Y')  ?>| Designed with love by <span class="text-danger">DesignCoon<span></p>
+            </div>
+             <div class="col-md-3 offset-sm-2"style="margin-top: 75px">
+              <img src="/img/16.png">
+            </div>
+          </div>
+</div>
+      </footer>
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
